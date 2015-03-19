c59b44c6ff00ef5fe665b829f6966c669b95e425
Gregory14 <gregory.joly.14@gmail.com>
2015-03-19 12:36:16 +0100

    Exo fonction somme en php

--- 06_php/02_rss/index.php
+++ 06_php/02_rss/index.php
@@ -5,6 +5,16 @@
 	<title>Document</title>
 </head>
 <body>
-
+<?php
+	$articles array(
+	"title",
+	"link",
+	"description",
+	"language",
+	"copyright",
+	"pubDate",
+	"item",
+	);
+?>
 </body>
 </html>
--- 06_php/index.php
+++ 06_php/index.php
@@ -6,49 +6,62 @@
 </head>
 <body>
 <?php 
-	// phpinfo(); 
+	// phpinfo();; 
 
-	echo "J'ai oublié la balise fermante PHP ! <br />";
-	echo "Que se passe-t-il ?<br />";
+	/*echo "J'ai oublié la balise fermante PHP ! <br />";;
+	echo "Que se passe-t-il ?<br />";;
 
-	$a = 1; 
+	$a = 1;; 
 	function test()
 	{
-		global $a;
-		echo $a;
+		global $a;;
+		echo $a;;
 	}
 
-	test();
+	test();;
 
-	$b = "Bonjour";
-	$$b = "Monde"; // $Bonjour = "Monde"
+	$b = "Bonjour";;
+	$$b = "Monde";; // $Bonjour = "Monde"
 
-	echo $b . "<br/>";
+	echo $b . "<br/>";;
 
-	echo $$b . "<br/>";
-	echo ${$b} . "<br/>";
-	echo $bonjour . "<br/>";
+	echo $$b . "<br/>";;
+	echo ${$b} . "<br/>";;
+	echo $bonjour . "<br/>";;
 
 	// Definir une constante
-	define("PI", 3.14);
-	echo PI. "<br/>";
-	echo pi. "<br/>";
+	define("PI", 3.14);;
+	echo PI. "<br/>";;
+	echo pi. "<br/>";;
 
-	const NOMCONSTANTE = "Valeur de la constante";
+	const NOMCONSTANTE = "Valeur de la constante";;
 
-var_dump($b);
+var_dump($b);;
 
 // Les tableaux
-$monTableau = array();
-var_dump($monTableau);
+$monTableau = array();;
+var_dump($monTableau);;
 
-$course = array ("fruit" => "fraise", "legume" => "tomate", "cereales" => "chocopops");
-var_dump($course);
+$course = array ("fruit" => "fraise", "legume" => "tomate", "cereales" => "chocopops");;
+var_dump($course);;
 
-$course ["Viande"] : "poulet"; 
-var_dump($course);
+$course ["Viande"] : "poulet";; 
+var_dump($course);;
 
-$course = array("kiwi", "poulet"); 
+$course = array("kiwi", "poulet");; */
+
+$nbr1 = 1;;
+$nbr2 = 2;;
+
+function somme($nbr1, $nbr2) {
+	/*global $a, $b ;;
+	$nbr2 = $nbr1 + $nbr2;;*/
+	return $nbr1 + $nbr2;;
+}
+
+echo somme(1 , 3);;
+/*somme ();;
+echo $nbr2;;*/
 
 ?>	
 </body>
